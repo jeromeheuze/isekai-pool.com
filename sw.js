734e1a0986863002f1f205a7ce4e38856e3818b5
Spectum3900 <jeromeheuze@gmail.com>
2025-09-06 18:59:14 -0700

    up

---
+++ sw.js
@@ -0,0 +1,50 @@
+// Service Worker for Isekai Pool
+// Basic service worker for future PWA features
+
+const CACHE_NAME = 'isekai-pool-v1';
+const urlsToCache = [
+    '/',
+    '/index.php',
+    '/yenten.php',
+    '/mining.php',
+    '/assets/css/main.css',
+    '/assets/js/main.js',
+    '/assets/js/mining.js'
+];
+
+// Install event
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                // Return cached version or fetch from network
+                return response || fetch(event.request);
+            }
+        )
+    );
+});
+
+// Activate event
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
